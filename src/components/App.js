1ea3dbc5491ceb166169fb7d7f885a02c22c145a
Abdul Moiz <moiza0799@gmail.com>
2021-11-17 22:32:53 +0500

    IPFS protocol added

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,52 @@
-import React, { Component } from 'react';
-import logo from '../logo.png';
-import './App.css';
+import React, { Component } from 'react'
+import logo from '../logo.png'
+import './App.css'
+import { create } from 'ipfs-http-client'
+
+
+const ipfs = create('https://mainnet.infura.io/v3/5680b098f95145999b986f9fffd90444:5001') // (the default in Node.js)
+
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      buffer: null,
+    }
+  }
+
+  captureFile = (event) => {
+    event.preventDefault()
+    //process file for IPFS
+    const file = event.target.files[0]
+    const reader = new window.FileReader()
+    reader.readAsArrayBuffer(file)
+
+    reader.onload = () => {
+      this.setState({ buffer: Buffer(reader.result) })
+    }
+
+    console.log(reader)
+  }
+  onSubmit = (event) => {
+    event.preventDefault()
+    ipfs.add(this.state.buffer, (error, result) => {
+      //do stuff here
+      console.log('ipfs-results',result);
+      if(error){
+        console.log(error);
+        return;
+      }
+    });
+   }
+
+  // onSubmit = async (event) => {
+  //   event.preventDefault()
+  //   console.log("submitting..")
+  //   const result = await ipfs.add(this.state.buffer)
+  //   console.log("ipfs", result)
+  // }
+
   render() {
     return (
       <div>
@@ -13,9 +57,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-          
-           Meme of the day
-          
+            Meme of the day
           </a>
         </nav>
         <div className="container-fluid mt-5">
@@ -39,15 +81,24 @@ class App extends Component {
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
+                  LEARN BLOCKCHAIN{' '}
+                  <u>
+                    <b>NOW! </b>
+                  </u>
                 </a>
+                <p>&nbsp;</p>
+                <h2>Change name </h2>
+                <form onSubmit={this.onSubmit}>
+                  <input type="file" onChange={this.captureFile}></input>
+                  <input type="submit"></input>
+                </form>
               </div>
             </main>
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
